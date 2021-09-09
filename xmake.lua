2e6a6dee4ed9123af08c341825d27685fde3344a
Louis Bailleau <louis.bailleau@free.fr>
2021-09-09 16:31:36 +0200

    Updated xmake configuration

--- xmake.lua
+++ xmake.lua
@@ -3,14 +3,14 @@
 -- =========================
 add_rules("mode.debug", "mode.release")
 
-add_repositories("local-repo xmake/repos")
-
-add_requires("glm", "glfw", "tinyobjloader", "vulkan-loader")
+add_requires("glm", "glfw", "tinyobjloader", "vulkan-headers", "vulkan-loader")
 
 target('littlevulkanengine')
     set_kind('binary')
     add_files('src/**.cpp')
     add_headerfiles('src/**.hpp')
 
-    add_packages("glm", "glfw", "tinyobjloader", "vulkan-loader")
+    set_languages("cxx20")
+
+    add_packages("glm", "glfw", "tinyobjloader", "vulkan-headers", "vulkan-loader")
     
