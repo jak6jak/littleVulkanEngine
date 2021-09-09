f11f2286424a78cb9306fe005d1916c604b27691
Louis Bailleau <louis.bailleau@free.fr>
2021-09-09 14:20:03 +0200

    Updated xmake configuration to use `vulkan-loader`

--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,16 @@
-add_requires("glm", "glfw", "tinyobjloader")
+-- =========================
+-- Project information:
+-- =========================
+add_rules("mode.debug", "mode.release")
+
+add_repositories("local-repo xmake/repos")
+
+add_requires("glm", "glfw", "tinyobjloader", "vulkan-loader")
 
 target('littlevulkanengine')
     set_kind('binary')
     add_files('src/**.cpp')
     add_headerfiles('src/**.hpp')
 
-    add_packages("glm", "glfw", "tinyobjloader")
-
-    add_syslinks("vulkan")
+    add_packages("glm", "glfw", "tinyobjloader", "vulkan-loader")
     
---
+++ xmake/repos/packages/v/vulkan-headers/xmake.lua
@@ -0,0 +1,24 @@
+package("vulkan-headers")
+
+    set_homepage("https://github.com/KhronosGroup/Vulkan-Headers/")
+    set_description("Vulkan Header files and API registry")
+    set_license("Apache-2.0")
+
+    add_urls("https://github.com/KhronosGroup/Vulkan-Headers/archive/$(version).tar.gz", {version = function (version) return version:startswith("v") and version or "sdk-" .. version:gsub("%+", ".") end})
+    add_versions("1.2.189+0", "472f1ee308b632d9b2c9f7094389c039a9842cec5c005932da8da65fa6d923ce")
+    add_versions("1.2.162+0", "eb0f6a79ac38e137f55a0e13641140e63b765c8ec717a65bf3904614ef754365")
+    add_versions("1.2.154+0", "a0528ade4dd3bd826b960ba4ccabc62e92ecedc3c70331b291e0a7671b3520f9")
+
+    add_deps("cmake")
+
+    on_install("windows", "linux", "macosx", function (package)
+        import("package.tools.cmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:check_csnippets({test = [[
+            void test() {
+                int version = VK_HEADER_VERSION;
+            }
+        ]]}, {includes = "vulkan/vulkan.h"}))
+    end)
